6de7ff6f1590789f5ed02f07611f7aaf9139e786
Tino Reyna <tinoreyna1984@gmail.com>
2023-10-18 23:22:44 -0500

    Cambios en la data de prueba

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,11 +20,11 @@ INSERT INTO clientes (nombre_cliente, apellidos_cliente, doc_id, fono_cliente, e
 INSERT INTO clientes (nombre_cliente, apellidos_cliente, doc_id, fono_cliente, email_cliente, estado_cliente) VALUES ('Roman', 'Bejar', '43117840', '922361024', 'rbejar@mail.com', 'CLIENTE');
 INSERT INTO clientes (nombre_cliente, apellidos_cliente, doc_id, fono_cliente, email_cliente, estado_cliente) VALUES ('Alberto', 'Light', '42889563', '921458200', 'alight@mail.com', 'CLIENTE');
 
-INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (2, 'INV0000000001', 'OTRO', curdate());
-INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (3, 'INV0000000002', 'OTRO', curdate());
-INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (4, 'INV0000000003', 'OTRO', curdate());
+INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (2, 'INV0000000001', 'OTRO', '2023-04-25');
+INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (3, 'INV0000000002', 'OTRO', '2023-02-11');
+INSERT INTO facturas (producto_id, cod_factura, forma_pago, fecha_pago) VALUES (4, 'INV0000000003', 'OTRO', '2023-05-23');
 
-INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000001', 'Venta del Nissan Sentra 2023', curdate(), 'CIERRE_GANADO', 1, 4, 2);
-INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000002', 'Kawasaki Z1000 2023', curdate(), 'CIERRE_GANADO', 2, 5, 2);
-INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000003', 'Hyundai Tucson 2024', curdate(), 'CIERRE_GANADO', 3, 6, 3);
+INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000001', 'Venta del Nissan Sentra 2023', '2023-02-12', 'CIERRE_GANADO', 1, 4, 2);
+INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000002', 'Kawasaki Z1000 2023', '2023-01-13', 'CIERRE_GANADO', 2, 5, 2);
+INSERT INTO ventas (orden_venta, desc_venta, fecha_reserva, estado_venta, factura_id, cliente_id, usuario_id) VALUES ('OV0000000003', 'Hyundai Tucson 2024', '2023-03-12', 'CIERRE_GANADO', 3, 6, 3);
 
