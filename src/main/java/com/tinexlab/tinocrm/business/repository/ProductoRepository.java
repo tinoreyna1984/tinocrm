aea35a2ac7c2bb5ce6b1ce510d25b3e676497a4b
Tino Reyna <tinoreyna1984@gmail.com>
2023-10-23 23:31:26 -0500

    - Se crea controlador para dashboard. - Se ajusta repositorio de productos para dashboard.

--- src/main/java/com/tinexlab/tinocrm/business/repository/ProductoRepository.java
+++ src/main/java/com/tinexlab/tinocrm/business/repository/ProductoRepository.java
@@ -2,6 +2,24 @@ package com.tinexlab.tinocrm.business.repository;
 
 import com.tinexlab.tinocrm.business.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    // lista de productos vendidos
+    @Query("SELECT p.nombreProducto AS producto, COUNT(*) AS numVentas " +
+            "FROM Producto p, Factura f " +
+            "WHERE p.id = f.producto.id " +
+            "GROUP BY p.nombreProducto " +
+            "ORDER BY COUNT(*) DESC")
+    List<Map<String, Object>> findProductosVendidos();
+
+    // total recaudado según las facturas
+    @Query("SELECT SUM(p.precioProducto) " +
+            "FROM Producto p, Factura f " +
+            "WHERE p.id = f.producto.id")
+    BigDecimal findTotalVendido();
 }
